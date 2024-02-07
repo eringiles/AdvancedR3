0a9c4d5d5a258d933adbe9132674c3c4e2287f2d
Erin Danielle Giles <eringile@umich.edu>
2024-02-06 23:58:03 -0500

    add graph to pipeline

--- R/functions.R
+++ R/functions.R
@@ -12,3 +12,16 @@ descriptive_stats <- function(data) {
             tidyselect::where(is.numeric), ~ round(.x, digits = 1)
         ))
 }
+
+#' Plot for basic distribution of metabolite data.
+#'
+#' @param data lipidomics dataset
+#'
+#' @return A ggplot2 plot object.
+#'
+plot_distributions <- function(data) {
+    data %>%
+        ggplot2::ggplot(ggplot2::aes(x = value)) +
+        ggplot2::geom_histogram() +
+        ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+}
